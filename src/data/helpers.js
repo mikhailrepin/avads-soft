85c9e273a9f463ad35ddd2dfb9f099e4ef3fbb9f
Mikhail Repin <migel72333@gmail.com>
2024-12-12 18:10:41 +0300

    ---

--- src/data/helpers.js
+++ src/data/helpers.js
@@ -3,12 +3,12 @@
 export const Helpers = {
   company: {
     name: "АВАДС",
-    phone: "+7 495 023 54 45",
+    phone: "+7 (999) 999-99-99",
     mail: {
       sails: "sales@avads.ru",
       info: "info@avads.ru",
       support: "support@avads.ru",
     },
-    location: "127018, г. Москва, Полковая ул., дом № 3",
+    location: "Company address",
   },
-}
+};
