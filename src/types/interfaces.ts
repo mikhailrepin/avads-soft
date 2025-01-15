143daba2413748736fa5a0cb280d4c0b04594599
Mikhail Repin <migel72333@gmail.com>
2025-01-15 21:47:56 +0300

    Refactor button and product card components; update styles and remove unused files
    - Updated ButtonGeneric component to use a more flexible props structure.
    - Refactored ProductCard component to align with new button structure and improve readability.
    - Enhanced MainHeader to highlight the current path in the navigation.
    - Added new styles for text gradients in base.css.
    - Removed deprecated products data files and the Grammar script.
    - Updated index.astro to utilize new product data structure and added a call-to-action section.

---
+++ src/types/interfaces.ts
@@ -0,0 +1,54 @@
+// Базовые типы для элементов
+export type Elem = {
+  Variant: "primary" | "secondary";
+  Size: "default" | "small" | "big";
+  Style: "filled" | "outlined" | "ghosted" | "hidden";
+  Target: "action" | "link";
+};
+
+// Базовый интерфейс для текстовых элементов
+export interface BaseText {
+  Title: string;
+  Subtitle?: string;
+  Description?: string;
+  Caption?: string;
+  CustomClass?: string;
+}
+
+// Базовый интерфейс для иконок
+export interface IconProps {
+  IconSprite: string;
+  IconVisible: boolean;
+}
+
+// Базовый интерфейс для кнопки
+export interface BaseButton {
+  Visible: boolean;
+  Variant?: Elem["Variant"];
+  Size?: Elem["Size"];
+  Style?: Elem["Style"];
+  Disabled?: boolean;
+  Loading?: boolean;
+  Toggle?: boolean;
+  Tooltip?: string;
+  IconLeft?: IconProps;
+  IconRight?: IconProps;
+  ButtonLabel: string;
+  ButtonLink?: string;
+  CustomClass?: string;
+  Onclick?: () => void;
+  Target?: Elem["Target"];
+}
+
+// Интерфейс для карточек
+export interface BaseCard extends BaseText {
+  Image: string;
+  Status?: string;
+  FirstButton?: BaseButton;
+  SecondButton?: BaseButton;
+}
+
+// Интерфейс для Call to Action
+export interface CallToAction extends BaseText {
+  ButtonProps?: BaseButton;
+}
